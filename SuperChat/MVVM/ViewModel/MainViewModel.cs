91ad1e99f01aed25dce4ad2640c0a0f08db306ac
Alvin Yanson <yansonalvin29@gmail.com>
2022-03-20 21:13:17 +0800

    done working in contacts and messages viewmodel

--- SuperChat/MVVM/ViewModel/MainViewModel.cs
+++ SuperChat/MVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SuperChat.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,51 @@ namespace SuperChat.MVVM.ViewModel
 {
     internal class MainViewModel
     {
+        public ObservableCollection<MessageModel> Messages  { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 6; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allison",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/i2szTsp.png",
+                    Messages = Messages
+                });
+            }
+        }
+
     }
 }
